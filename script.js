6ac81e122b5cebf4e5047714dc665dac657c85a9
Ryan DeGroot <degrootr253@gmail.com>
2021-09-13 20:52:33 -0700

    start of displaying event cards

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var cityName = document.getElementById('city')
+var eventEl = document.getElementById("events-container")
 
 var apiRequest = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=15a2f6e975005b96c0df56340849949d";
 
@@ -19,3 +20,57 @@ function getWeather() {
     });
 }
 document.getElementById("city").addEventListener("click", getWeather);
+
+function displayEvents(data) {
+  if (data.length === 0) {
+    repoContainerEl.textContent = 'No events found.';
+    return;
+  };
+
+  for (var i = 0; i < data.length; i++){
+    // Need event API data for this
+    var imgData = data[i]...
+    var eventTitle = data[i]...
+    var description = data[i]...
+
+    //Create card div
+    var card = document.createElement('div');
+    card.setAttribute('class', 'card');
+    
+    //Create img element for card
+    var img = document.createElement('img');
+    img.setAttribute('src', imgData);
+
+    // Create div container for event name and description in card.
+    var container = document.createElement('div');
+    container.setAttribute('class', 'container');
+    
+    // Create event name element
+    var eventName = document.createElement('h4');
+    eventName.setAttribute('class', 'event-title');
+    eventName.textContent = eventTitle;
+    
+    // Create description element
+    var eventDescrip = document.createElement('p');
+    eventDescrip.setAttribute('class', 'event-description');
+    eventDescrip.textContent = description;
+    
+
+    //Append elements for HTML
+    card.appendChild(eventEl);
+    img.appendChild(card);
+    container.appendChild(card);
+    eventName.appendChild(container);
+    eventDescrip.appendChild(container);
+  };
+
+  
+  
+
+  
+  
+  
+  
+  
+
+}
